420541b25cef5219daf7d934584ae4cc4176c464
ggutu <151479796+ggutu@users.noreply.github.com>
2024-06-07 15:44:47 -0500

    Create newvpc.tf

---
+++ newvpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "172.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc-terraform"
+    Purpose = "Jenkins Terraform Demo"
+  }
+}
